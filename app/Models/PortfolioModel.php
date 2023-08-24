3a9e394449afc16c03db21dfaa1927629f08270a
AntonelloStefania <antonelloste94@gmail.com>
2023-08-24 19:19:40 +0200

    [add] functions to models

--- app/Models/PortfolioModel.php
+++ app/Models/PortfolioModel.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Types;
 
 class PortfolioModel extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','front_ender','back_ender','ui','ux','illustrator','image','description','link'];
+    protected $fillable = ['title','front_ender','back_ender','ui','ux','illustrator','image','description','link','type_id'];
+
+    public function types(){
+        return $this->belongsTo(Types::class);
+    }
+
 }
--- app/Models/Types.php
+++ app/Models/Types.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\PortfolioModel;
 
 class Types extends Model
 {
     use HasFactory;
+    protected $fillable=['category', 'mobile', 'tablet', 'pc', 'smart_tv','windows','linux','mac','android','ios','age_restriction'];
+
+
+    public function portfolio_models(){
+        return $this->hasMany(PortfolioModel::class);
+    }
 }
