5668d620d4f775f65b850c2af169ce9366b89997
AntonelloStefania <antonelloste94@gmail.com>
2023-08-24 19:19:40 +0200

    launched types seeder

--- database/seeders/TypesSeeder.php
+++ database/seeders/TypesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Types;
+use Illuminate\Support\Facades\DB;
 
 class TypesSeeder extends Seeder
 {
@@ -14,6 +16,26 @@ class TypesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ['eCommerce','Magazine','Blog','Portal'];
+
+        foreach($categories as $category){
+            $type = new Types();
+
+            $type->category= $category;
+           
+            DB::table('types')->insert([
+                'category'=>$category,
+                'mobile'=>false,
+                'tablet'=>false,
+                'pc'=>false,
+                'smart_tv'=>false,
+                'windows'=>false,
+                'linux'=>false,
+                'mac'=>false,
+                'android'=>false,
+                'ios'=>false,
+                'age_restriction'=>false,
+            ]);
+        }
     }
 }
